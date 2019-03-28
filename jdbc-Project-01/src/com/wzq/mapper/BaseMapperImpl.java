50a83dbe65747e56bf57bc19a0a746c966e04588
jerry <1344731479@qq.com>
2019-03-28 08:24:32 +0800

    数据库元数据操作-获取数据库信息（版本号，用户名，表数据等）

---
+++ jdbc-Project-01/src/com/wzq/mapper/BaseMapperImpl.java
@@ -0,0 +1,120 @@
+package com.wzq.mapper;
+
+import com.wzq.util.JdbcUtil;
+import com.wzq.util.ReflectUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BaseMapperImpl{
+
+    public static <T> T get(Class<T> clazz, String sql, boolean mapUnderscoreToCamelCase, Object... args) {
+        T entity = null;
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = JdbcUtil.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                preparedStatement.setObject(i + 1, args[i]);
+            }
+            resultSet = preparedStatement.executeQuery();
+
+            Map<String, Object> values = new HashMap<>();
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            if (resultSet.next()) {
+                for (int i = 0; i < metaData.getColumnCount(); i++) {
+                    String columnLabel = metaData.getColumnLabel(i + 1);
+                    Object object = resultSet.getObject(columnLabel);
+                    values.put(columnLabel, object);
+                }
+            }
+            //利用反射创建对象
+            entity = clazz.newInstance();
+            //通过解析SQL语句来判断到底选择了那些列，以及需要为entity对象的那些属性赋值
+            for (Map.Entry<String, Object> entry : values.entrySet()) {
+                String key = entry.getKey();
+                Object value = entry.getValue();
+                ReflectUtil.invoke(entity, key, value, mapUnderscoreToCamelCase);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JdbcUtil.release(connection, preparedStatement, resultSet);
+        }
+        return entity;
+    }
+
+    public static <T> List<T> getForList(Class<T> clazz, String sql, Object ... args){
+        List<T> list = new ArrayList<>();
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = JdbcUtil.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                preparedStatement.setObject(i + 1, args[i]);
+            }
+            resultSet = preparedStatement.executeQuery();
+
+            List<String> columnLabels = getColunmLabels(resultSet);
+            List<Map<String, Object>> values = resultSetToMapList(resultSet, columnLabels);
+            //通过解析SQL语句来判断到底选择了那些列，以及需要为entity对象的那些属性赋值
+            list = transferMapListToBeanList(clazz, values);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JdbcUtil.release(connection, preparedStatement, resultSet);
+        }
+
+        return list;
+    }
+
+    private static <T> List<T> transferMapListToBeanList(Class<T> clazz, List<Map<String, Object>> values) throws InstantiationException, IllegalAccessException {
+        List<T> list = new ArrayList<>();
+        T entity;
+        if(!values.isEmpty()){
+            for (Map<String, Object> value : values) {
+                entity = clazz.newInstance();
+                for (Map.Entry<String, Object> entry : value.entrySet()) {
+                    String k = entry.getKey();
+                    Object v = entry.getValue();
+                    ReflectUtil.invoke(entity, k, v);
+                }
+                list.add(entity);
+            }
+        }
+        return list;
+    }
+
+    private static List<Map<String, Object>> resultSetToMapList(ResultSet resultSet, List<String> columnLabels) throws SQLException {
+        List<Map<String, Object>> values = new ArrayList<>();
+        while (resultSet.next()) {
+            Map<String, Object> entity = new HashMap<>();
+            for (String column : columnLabels) {
+                Object value = resultSet.getObject(column);
+                entity.put(column, value);
+            }
+            values.add(entity);
+        }
+        return values;
+    }
+
+    private static List<String> getColunmLabels(ResultSet resultSet) throws SQLException {
+        List<String> labels = new ArrayList<>();
+        ResultSetMetaData metaData = resultSet.getMetaData();
+        for(int i=0; i<metaData.getColumnCount(); i++){
+            labels.add(metaData.getColumnLabel(i+1));
+        }
+        return labels;
+    }
+
+}
--- jdbc-Project-01/src/com/wzq/test/JdbcTest.java
+++ jdbc-Project-01/src/com/wzq/test/JdbcTest.java
@@ -2,9 +2,11 @@ package com.wzq.test;
 
 import com.wzq.entity.Member;
 import com.wzq.entity.Province;
-import com.wzq.mapper.BaseMapper;
+import com.wzq.mapper.BaseMapperImpl;
+import com.wzq.util.JdbcUtil;
 import org.junit.Test;
 
+import java.sql.*;
 import java.util.List;
 
 public class JdbcTest {
@@ -13,7 +15,7 @@ public class JdbcTest {
     public void test01(){
         String sql = "select id, name from member where name=?";
         Object[] objects = {"Bob"};
-        Member member = BaseMapper.get(Member.class, sql, false, objects);
+        Member member = BaseMapperImpl.get(Member.class, sql, false, objects);
         System.err.println(member.toString());
     }
 
@@ -22,7 +24,7 @@ public class JdbcTest {
         String sql = "select id, province_code provinceCode, province_name provinceName from province " +
                 "where province_name=?";
         Object[] objects = {"江西"};
-        Province province = BaseMapper.get(Province.class, sql, false, objects);
+        Province province = BaseMapperImpl.get(Province.class, sql, false, objects);
         System.err.println(province.toString());
     }
 
@@ -31,7 +33,7 @@ public class JdbcTest {
         String sql = "select id, province_code, province_name from province " +
                 "where province_name=?";
         Object[] objects = {"江西"};
-        Province province = BaseMapper.get(Province.class, sql, true, objects);
+        Province province = BaseMapperImpl.get(Province.class, sql, true, objects);
         System.err.println(province.toString());
     }
 
@@ -40,8 +42,72 @@ public class JdbcTest {
         String sql = "select id, province_code provinceCode, province_name provinceName from province " +
                 "where province_name like ?";
         Object[] objects = {"%西%"};
-        List<Province> provinces = BaseMapper.getForList(Province.class, sql, objects);
+        List<Province> provinces = BaseMapperImpl.getForList(Province.class, sql, objects);
         System.err.println(provinces);
+    }
+
+    @Test
+    public void testDataBaseMetaData(){
+        Connection connection = null;
+        DatabaseMetaData data = null;
+        ResultSet resultSet = null;
+        try{
+            connection = JdbcUtil.getConnection();
+            data = connection.getMetaData();
+            //可以得到数据库本身的一些基本信息
+            //得到数据库的版本号
+            int databaseMajorVersion = data.getDatabaseMajorVersion();
+            System.err.println("数据库版本号：" + databaseMajorVersion);
+
+            //得到连接到数据库的用户名
+            String userName = data.getUserName();
+            System.err.println(userName);
+
+            //得到MySQL中有哪些数据库
+            resultSet = data.getCatalogs();
+            while(resultSet.next()){
+                System.err.println(resultSet.getString(1));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            JdbcUtil.release(connection, null, resultSet);
+        }
+    }
+
+    /**
+     * 结果集的元数据
+     */
+    @Test
+    public void testResultSetMetaData(){
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try{
+            connection = JdbcUtil.getConnection();
+            String sql = "select id, province_code provinceCode, province_name provinceName from province";
+            preparedStatement = connection.prepareStatement(sql);
+            resultSet = preparedStatement.executeQuery();
+
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            //获取列的个数
+            int columnCount = metaData.getColumnCount();
+            System.err.println(columnCount);
+
+            for(int i=0; i<columnCount; i++){
+                //列名
+                String columnName = metaData.getColumnName(i + 1);
+                //别名
+                String columnLabel = metaData.getColumnLabel(i + 1);
+                System.err.println(columnName + "--" + columnLabel);
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            JdbcUtil.release(connection, preparedStatement, resultSet);
+        }
     }
 
 
